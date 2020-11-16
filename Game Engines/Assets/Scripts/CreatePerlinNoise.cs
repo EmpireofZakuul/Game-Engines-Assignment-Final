3c0d1b56dfef2fd588ea16392a9b00a0a91b4d10
EmpireofZakuul <60693663+EmpireofZakuul@users.noreply.github.com>
2020-11-16 19:04:57 +0000

    Started On perlin noise texture for city

--- Game Engines/Assets/Scripts/CreatePerlinNoise.cs
+++ Game Engines/Assets/Scripts/CreatePerlinNoise.cs
@@ -4,15 +4,65 @@ using UnityEngine;
 
 public class CreatePerlinNoise : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Perlin Noise Image Size")]
+    public int perlinImageWidth = 256;
+    public int perlinImageHeight = 256;
+    public static CreatePerlinNoise instance = null;
+    [Header("Perlin Noise ")]
+    public Vector2 offsetOfThePerlinNoiseTexture;
+    public bool randomizeTexture;
+    public float scaleOfTheNoise = 1f;
+
+
+
+    void OnEnable()
     {
-        
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = GeneratePerlinTexture();
     }
 
-    // Update is called once per frame
-    void Update()
+    public Texture2D GeneratePerlinTexture()
     {
-        
+        if (randomizeTexture)
+        {
+            offsetOfThePerlinNoiseTexture = new Vector2(Random.Range(0, 99999), Random.Range(0, 99999));
+        }
+
+        Texture2D texture = new Texture2D(perlinImageWidth, perlinImageHeight);
+
+        for (int x = 0; x < perlinImageWidth; x++)
+        {
+            for (int y = 0; y < perlinImageHeight; y++)
+            {
+                texture.SetPixel(x, y, Noise(x, y));
+            }
+        }
+
+        texture.Apply();
+
+        return texture;
     }
+
+    Color Noise(int x, int y)
+    {
+        float XCoordinate = (float)x / perlinImageWidth * scaleOfTheNoise + offsetOfThePerlinNoiseTexture.x;
+        float YCoordinate = (float)y / perlinImageHeight * scaleOfTheNoise + offsetOfThePerlinNoiseTexture.y;
+
+        float sampled = Mathf.PerlinNoise(XCoordinate, YCoordinate);
+        Color ColorOfThePerlin = new Color(sampled, sampled, sampled);
+
+        return ColorOfThePerlin;
+    }
+
+
 }
