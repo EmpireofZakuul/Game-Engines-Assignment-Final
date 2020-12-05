d55c7060089704dddd65739bde346fe730da3579
EmpireofZakuul <60693663+EmpireofZakuul@users.noreply.github.com>
2020-12-05 21:12:21 +0000

    created the greyscale script for menu

---
+++ Game Engines/Assets/Scripts/TurningGreyScaleOnOff.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurningGreyScaleOnOff : MonoBehaviour
+{
+     public grey ascript;
+      public bool greyed = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+         if (greyed)
+            {
+                ascript.enabled = false;
+            }
+            else
+            {
+                 ascript.enabled = true;
+            }
+    }
+}
